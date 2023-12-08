b68b7482e622a0e3c24adf29f4c37b6a3a62fb07
Marisol <s.min.g7904@gmail.com>
2023-12-08 13:13:00 +0900

    no message

--- src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
@@ -3,9 +3,8 @@ package com.fastcampus.javaoop.logic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BubbleSort <T extends Comparable<T>> implements Sort<T> {
+public class BubbleSort <T extends Comparable<T>> {
 
-    @Override
     public List<T> sort(List<T> input){
         List<T> output = new ArrayList<>(input);
 
--- src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort <T extends Comparable<T>> implements Sort<T> {
+public class JavaSort <T extends Comparable<T>> {
 
-    @Override
     public List<T> sort(List<T> list){
         List<T> output = new ArrayList<>(list);
         Collections.sort(output);
